f766ddba9809a1dc6140e7d415b8f8464fbb4990
unknown <DougMDev@outlook.com>
2022-06-27 13:28:09 +0100

    Initial routes setup, test post request for movies added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 3000;
 
 const app = express();
 
+let movies = [];
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Routes
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to thee World of Movies!");
+});
+//Specific Routes
+app.post("/movies", (req, res) => {
+  const movie = req.body;
+  console.log(movie);
+  movies.push(movie);
+
+  res.send("Movie has been added to the database");
 });
 
+//port listener
 app.listen(port, (req, res) => {
   console.log(`Listening on Port ${port}`);
 });
