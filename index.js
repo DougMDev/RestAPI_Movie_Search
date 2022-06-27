284cd50255283af69aa735d7c4af111b87621e34
unknown <DougMDev@outlook.com>
2022-06-27 13:41:44 +0100

    Form added and successfully links back and fills array on submit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = 3000;
+const path = require("path");
 
 const app = express();
 
@@ -10,10 +11,18 @@ let movies = [];
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
 
 //Routes
 app.get("/", (req, res) => {
   res.send("Welcome to thee World of Movies!");
+});
+
+app.get("/newmovie", (req, res) => {
+  res.render("new-movie");
 });
 //Specific Routes
 app.post("/movies", (req, res) => {
